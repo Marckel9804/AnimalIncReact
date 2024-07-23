bbeddb1a66874506b3891c5b94a9011e7af5d447
JEONYUTAK <btt0408@gmail.com>
2024-07-23 21:25:59 +0900

    bm

---
+++ src/game/page/maingame/MyInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyInfo() {
+  return <div className="nes-container myinfo-container"></div>;
+}
+
+export default MyInfo;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainGame from "./game/page/maingame/MainGame.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/game/:room_id" element={<MainGame />} />
+    </Routes>
+  </BrowserRouter>
+);
