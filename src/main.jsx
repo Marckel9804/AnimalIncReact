aa836dbf9402cad946b12c353c788519c5c6bc08
Marckel9804 <ckdgh10421@gmail.com>
2024-08-02 10:28:51 +0900

    router jsx main jsx fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,37 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// <<<<<<< HEAD
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import MainGame from "./game/page/maingame/MainGame.jsx";
-// import Login from "./login/Login.jsx";
-// import Register from "./login/Register.jsx";
-// import BoardListPage from "./community/page/BoardListPage.jsx";
-// import BoardDetailPage from "./community/page/BoardDetailPage.jsx";
-// import BoardUpdatePage from "./community/page/BoardUpdatePage.jsx";
-// import BoardWritePage from "./community/page/BoardWritePage.jsx";
-//
-// function BoardWritepage() {
-//     return null;
-// }
-//
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <BrowserRouter>
-//     <Routes>
-//         <Route path="/game/:room_id" element={<MainGame />} />
-//
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//
-//
-//
-//     </Routes>
-//   </BrowserRouter>
-// =======
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-// >>>>>>> dev
 );
